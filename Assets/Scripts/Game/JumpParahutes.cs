316482447a7dcd8afce1e69e4333a13428fee6a4
IRONKAGE <IRONKAGE@gmail.com>
2020-06-07 15:48:16 +0300

    add parashutist but not completed

---
+++ Assets/Scripts/Game/JumpParahutes.cs
@@ -0,0 +1,20 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parashute : MonoBehaviour
+{
+    public Transform parashuteSpawn;
+    public Rigidbody ParashuteMan;
+    public float parashuteSpeed;
+
+    // Update is called once per frame
+    void Update() {
+        if(Input.GetButtonDown("Parashute")) {
+            Rigidbody ParashuteManRigidbody;
+            ParashuteManRigidbody = Instantiate(ParashuteMan, parashuteSpawn.position, parashuteSpawn.rotation) as Rigidbody;
+            ParashuteManRigidbody.AddForce(parashuteSpawn.forward * parashuteSpeed);
+        }
+        
+    }
+}
