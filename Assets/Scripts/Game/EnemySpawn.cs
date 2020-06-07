a594a52fa8ee010308fb3ea4ed3178fb1077ea4c
vadvoice <shev4enko.vadym@gmail.com>
2020-06-07 11:54:38 +0300

    feature/enemy-spawner: just put enemies in random place on the map every 5 sedonds

---
+++ Assets/Scripts/Game/EnemySpawn.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public GameObject enemy;
+    float randX;
+    float randY;
+    Vector2 whereToSpawn;
+    public float spawnRate = 2f;
+    float nextSpawn = 0.0f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            randX = Random.Range(-2.7f, 2.7f);
+            randY = Random.Range(40f, 140f);
+
+            whereToSpawn = new Vector2(randX, randY);
+            Instantiate(enemy, whereToSpawn, Quaternion.identity);
+        }
+    }
+}
