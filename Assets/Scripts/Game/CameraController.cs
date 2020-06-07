bbdddd559fa1fc26b703f6671119ac6ee2824939
vadvoice <shev4enko.vadym@gmail.com>
2020-06-07 11:10:01 +0300

    feature/bullet: just make an prefabs for fire making from the starship

--- Assets/Scripts/Game/CameraController.cs
+++ Assets/Scripts/Game/CameraController.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 public class CameraController : MonoBehaviour
 {
     //Camera movemant
-    private float movingSpeed = 5f;
+    private float movingSpeed = 0f;
 
     // timer
     public static float timer;
